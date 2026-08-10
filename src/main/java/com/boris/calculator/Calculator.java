7646d95b10b9d5930e97ca506b13089d2b7204d5
KBoris <borisnell2006@yahoo.fr>
2026-08-10 19:09:39 +0200

    Fix multiplication

--- src/main/java/com/boris/calculator/Calculator.java
+++ src/main/java/com/boris/calculator/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
     }
 
     public double multiplication(double a, double b) {
-        return a + b;
+        return a * b;
     }
 
     public double division(double a, double b) {
